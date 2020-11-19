8931c85589e03eb0ebf9492ac04b2c05f45a6783
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-19 11:16:11 +0200

    init

---
+++ StringsDemo.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.strings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+
+    class StringsDemo
+    {
+        static void Main(string[] args)
+        {
+            string a = "abc";
+            string b = "abcd";
+           // Console.WriteLine( a < b);
+            // <, > operators can not be applied to strings-we can not compare tehm like that
+            Console.WriteLine(a == b);
+        } 
+    }
+}
